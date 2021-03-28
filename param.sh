bdc060b69fe61550e3f28f420e4062ac0c60202c
Ramseez <77793406+Ramseez@users.noreply.github.com>
2021-03-28 16:03:46 -0400

    Delete param.sh

--- param.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# capture the current value of the parameter into oldvalue
-cat /home/ubuntu/kegman.json | jq -r .`cat /home/ubuntu/changeparam/param` > /home/ubuntu/changeparam/oldvalue
