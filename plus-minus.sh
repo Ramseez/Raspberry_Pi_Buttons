78e416189bab18e8a24b81598e8e7213e1770156
Ramseez <77793406+Ramseez@users.noreply.github.com>
2021-03-28 16:04:09 -0400

    Delete plus-minus.sh

--- plus-minus.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# use bc to add (or subtract) the delta against the oldvalue and store as newvalue no quotes
-echo `cat /home/ubuntu/changeparam/oldvalue``cat /home/ubuntu/changeparam/plus-minus``cat /home/ubuntu/changeparam/delta` | bc > /home/ubuntu/changeparam/newvalue
